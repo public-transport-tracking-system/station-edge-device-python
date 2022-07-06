16ef8d87465603b08ed8ac37fec9799eeb815bda
Daniel Klinkert <daniel.klinkert@gmail.com>
2022-07-06 08:24:47 +0200

    working with threads WIP

--- sensorData.py
+++ sensorData.py
@@ -3,9 +3,22 @@ import random
 import time
 from Sensor import Sensor
 from Station import Station
+from threading import Thread, Event
+from queue import Queue
+import itertools
+import json
+import threading
+from os import system
+import sys
+
 
 class SensorData:
-    def mockData():
+    def __init__(self):
+        self = self
+        self.event = Event()
+        self.dataToSend = {}
+
+    def mockData(self):
         bus100 = Sensor("100", randint(10, 60))
         bus101 = Sensor("101", randint(10, 60))
         bus102 = Sensor("102", randint(10, 60))
@@ -17,4 +30,28 @@ class SensorData:
         route4 = Station("103", randint(10, 15), int(time.time()), bus103)
         items = [route1, route2, route3, route4]
         return random.choice(items)
+
+    def modify_variable(self, queue_out):
+        while not self.event.wait(2):
+            currentRoute = self.mockData()
+            queue_out.put(currentRoute)
         
+    def displayData(self, queue):
+        for sequence in itertools.count():
+            data = queue.queue[0]
+            if data.dataFromSensor.routeId in self.dataToSend:
+                count = self.dataToSend.get(data.dataFromSensor.routeId)
+                count += 1
+                self.dataToSend[data.dataFromSensor.routeId] = count
+            else:
+                self.dataToSend[data.dataFromSensor.routeId] = 1
+            for k, v in self.dataToSend.items():
+                number = v
+                print ("{:<8} {:<15}".format(k, number))
+            
+
+    def startDataGeneration(self, queue):
+        t = threading.Timer(1, self.modify_variable, args=(queue,))
+        t.start()
+        t2 = threading.Timer(1, self.displayData, args=(queue,))
+        t2.start()
